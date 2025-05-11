8f1f3f8f7385fd2a7a22865b105f6b5a7cf3d8f1
Rocco DeStefano <Rocco.DeStefano.882@loyola.hcdsb.org>
2025-05-10 20:09:28 -0400

    Initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import "./Home.css"
+import LinkedIn_Icon from "../assets/LinkedIn-Icon.png"
+
+function Home() {
+    return (
+    <div className="home-container">
+        <h1>Rocco DeStefano</h1>
+        <h3>Computer Science Student at Queen's University</h3>
+    </div>
+    )
+  }
+  
+  export default Home  
