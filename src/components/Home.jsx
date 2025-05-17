ff4c3364ef59b106a1f0861489a58b565541e76e
Rocco DeStefano <Rocco.DeStefano.882@loyola.hcdsb.org>
2025-05-17 03:52:02 -0400

    Added About page and seperate files for the navigation bar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,13 @@
-import "./Home.css"
-import LinkedIn_Icon from "../assets/LinkedIn-Icon.png"
-import GitHub_Icon from "../assets/GitHub-Icon.png"
-import Mail_Icon from "../assets/Mail-Icon.png"
+import "./Home.css";
+import LinkedIn_Icon from "../assets/LinkedIn-Icon.png";
+import GitHub_Icon from "../assets/GitHub-Icon.png";
+import Mail_Icon from "../assets/Mail-Icon.png";
 
 function Home() {
     return (
     <div className="home-container">
-        <div className="navbar">
-            <nav>
-                <a href="/">Home</a>
-                <a href="/">About</a>
-                <a href="/">Portfolio</a>
-                <a href="/">Projects</a>
-                <a href="/">Contact</a>
-            </nav>
-        </div>
-        
-        <h1>Rocco DeStefano</h1>
-        <h3>Computer Science Student at Queen's University</h3>
+        <h1 style={{ lineHeight: "0", textShadow: "1px 1px 3px rgba(0, 0, 0, 0.5)" }}>Rocco DeStefano</h1>
+        <h3 style={{ lineHeight: "0", textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)" }}>Computer Science Student at Queen's University</h3>
         
         <div className="icons">
             <a href="https://www.linkedin.com/in/rocco-destefano-908a7932a/" target="_blank" rel="noopener noreferrer">
