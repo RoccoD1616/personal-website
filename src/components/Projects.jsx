bed48f521512bfabd432621924d271d8683211e3
Rocco DeStefano <Rocco.DeStefano.882@loyola.hcdsb.org>
2026-02-09 12:29:18 -0500

    Added VoiceCare AI to projects, reordered projects by date

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,20 +5,11 @@ import CompsaApp from "../assets/COMPSA-App.jpg";
 import RoomBooking from "../assets/Room-Booking.png";
 import CompsaCCR from "../assets/COMPSA-CCR.png";
 import NumNum from "../assets/NumNum.jpeg";
-import StudentBT from "../assets/Student-BT.jpg";
 import IsabelBalderClub from "../assets/Isabel-Balder-Club.png";
 import Britnell from "../assets/Britnell-Logo.png";
+import VoiceCareAI from "../assets/VoiceCare-AI.jpeg";
 
 const projects = [
-    {
-        title: "Personal Website",
-        date: "07/2025",
-        image: PersonalWebsite,
-        description: "Created my own personal website where others can learn about me, my experiences, and the skills that I've developed.",
-        technologies: ["HTML", "CSS", "JavaScript", "React"],
-        link: "https://github.com/RoccoD1616/personal-website",
-        alt: "Rocco DeStefano's Personal Website"
-    },
     {
         title: "COMPSA App",
         date: "In Progress",
@@ -38,6 +29,15 @@ const projects = [
         alt: "Computing Clubs Roundtable"
     },
     {
+        title: "VoiceCare AI",
+        date: "02/2026",
+        image: VoiceCareAI,
+        description: "A speech-driven Chrome extension that simplifies web navigation through conversational AI and guided on-screen assistance.",
+        technologies: ["HTML", "CSS", "JavaScript", "Gemini API", "Gradium"],
+        link: "https://github.com/NigelGroen5/voicecare-AI",
+        alt: "VoiceCare AI"
+    },
+    {
         title: "Room Booking",
         date: "08/2025",
         image: RoomBooking,
@@ -47,58 +47,22 @@ const projects = [
         alt: "Room Booking"
     },
     {
-        title: "NumNum",
-        date: "02/2025",
-        image: NumNum,
-        description: "An AI powered dining app that recommends restaurant choices to users based on their preferences, such as location, dietary needs, cuisine, price range and much more.",
-        technologies: ["Python", "Flask", "REST APIs"],
-        link: "https://devpost.com/software/numnum",
-        alt: "NumNum"
-    },
-    {
-        title: "Student Buy & Trade",
-        date: "04/2025",
-        image: StudentBT,
-        description: "Contributed to developing a platform where students can easily buy, sell, trade, and donate school items.",
+        title: "Personal Website",
+        date: "08/2025",
+        image: PersonalWebsite,
+        description: "Created my own personal website where others can learn about me, my experiences, and the skills that I've developed.",
         technologies: ["HTML", "CSS", "JavaScript", "React"],
-        link: "https://github.com/Queens-ESSDEV/Student-BST",
-        alt: "Student Buy & Trade"
+        link: "https://github.com/RoccoD1616/personal-website",
+        alt: "Rocco DeStefano's Personal Website"
     },
     {
-        title: "Isabel Balder Club Website",
-        date: "04/2025",
-        image: IsabelBalderClub,
-        description: "Developed a website for a Queen’s University theatre club, providing students with a centralized place to learn about the team and explore upcoming initiatives.",
-        technologies: ["HTML", "CSS", "TypeScript", "React"],
-        link: "https://github.com/queens-web-development-club/ws25t01-isabeltheatreclub",
-        alt: "Isabel Balder Club"
-    },
-    {
-        title: "Warehouse Optimizer",
-        date: "05/2025",
+        title: "Pending & Partial Automator",
+        date: "08/2025",
         image: Britnell,
-        description: "Developed a warehouse inventory optimization system using a frequency-based algorithm to analyze order quantities and improve item placement.",
-        technologies: ["Python", "MySQL ", "React", "Flask", "REST APIs"],
-        link: "javascript:void(0)",
-        alt: "Warehouse Optimizer"
-    },
-    {
-        title: "NSS Generator",
-        date: "06/2025",
-        image: Britnell,
-        description: "Built an automated system to generate sales metrics and monitor warehouse performance using APIs.",
+        description: "Built an automated system that emails customers regarding their pending orders and ETAs and logs all order data into internal company records.",
         technologies: ["Python", "MySQL"],
         link: "javascript:void(0)",
-        alt: "NSS Generator"
-    },
-    {
-        title: "NSS Report Automator",
-        date: "07/2025",
-        image: Britnell,
-        description: "Developed an automated system that calculates daily sales and sends summary reports via email using APIs.",
-        technologies: ["Python", "MySQL"],
-        link: "javascript:void(0)",
-        alt: "NSS Report Automator"
+        alt: "Backup Delete Automator"
     },
     {
         title: "Backup Delete Automator",
@@ -110,14 +74,50 @@ const projects = [
         alt: "Backup Delete Automator"
     },
     {
-        title: "Pending & Partial Automator",
-        date: "08/2025",
+        title: "NSS Report Automator",
+        date: "07/2025",
         image: Britnell,
-        description: "Built an automated system that emails customers regarding their pending orders and ETAs and logs all order data into internal company records.",
+        description: "Developed an automated system that calculates daily sales and sends summary reports via email using APIs.",
         technologies: ["Python", "MySQL"],
         link: "javascript:void(0)",
-        alt: "Backup Delete Automator"
-    }
+        alt: "NSS Report Automator"
+    },
+    {
+        title: "NSS Generator",
+        date: "06/2025",
+        image: Britnell,
+        description: "Built an automated system to generate sales metrics and monitor warehouse performance using APIs.",
+        technologies: ["Python", "MySQL"],
+        link: "javascript:void(0)",
+        alt: "NSS Generator"
+    },
+    {
+        title: "Warehouse Optimizer",
+        date: "05/2025",
+        image: Britnell,
+        description: "Developed a warehouse inventory optimization system using a frequency-based algorithm to analyze order quantities and improve item placement.",
+        technologies: ["Python", "MySQL ", "React", "Flask", "REST APIs"],
+        link: "javascript:void(0)",
+        alt: "Warehouse Optimizer"
+    },
+    {
+        title: "Isabel Balder Club Website",
+        date: "04/2025",
+        image: IsabelBalderClub,
+        description: "Developed a website for a Queen’s University theatre club, providing students with a centralized place to learn about the team and explore upcoming initiatives.",
+        technologies: ["HTML", "CSS", "TypeScript", "React"],
+        link: "https://github.com/queens-web-development-club/ws25t01-isabeltheatreclub",
+        alt: "Isabel Balder Club"
+    },
+    {
+        title: "NumNum",
+        date: "02/2025",
+        image: NumNum,
+        description: "An AI powered dining app that recommends restaurant choices to users based on their preferences, such as location, dietary needs, cuisine, price range and much more.",
+        technologies: ["Python", "Flask", "Gemini API"],
+        link: "https://devpost.com/software/numnum",
+        alt: "NumNum"
+    },
   ];
 
 function Projects() {
